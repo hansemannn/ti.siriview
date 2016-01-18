dbbde1f61315c9cae62399316c791812d50bc00a
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2016-01-18 19:53:52 +0100

    Basic support, complete API coverage

--- iphone/Classes/TiSiriviewModule.m
+++ iphone/Classes/TiSiriviewModule.m
@@ -87,21 +87,5 @@
 
 #pragma Public APIs
 
--(id)example:(id)args
-{
-	// example method
-	return @"hello world";
-}
-
--(id)exampleProp
-{
-	// example property getter
-	return @"hello world";
-}
-
--(void)setExampleProp:(id)value
-{
-	// example property setter
-}
 
 @end
